1d2325141fa00baebb2ff96f435af59561297f56
Archasmiel <archasmiel@gmail.com>
2025-05-19 10:34:41 +0300

    Backend start

---
+++ backend/src/main/kotlin/net/archasmiel/dnd5backend/Dnd5backendApplication.kt
@@ -0,0 +1,11 @@
+package net.archasmiel.dnd5backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Dnd5backendApplication
+
+fun main(args: Array<String>) {
+	runApplication<Dnd5backendApplication>(*args)
+}
---
+++ backend/src/test/kotlin/net/archasmiel/dnd5backend/Dnd5backendApplicationTests.kt
@@ -0,0 +1,13 @@
+package net.archasmiel.dnd5backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Dnd5backendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
